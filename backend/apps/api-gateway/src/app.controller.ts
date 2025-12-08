1e369af65d71803d0d81701b266f3e48cab0b58a
Nguyen1976 <nguyen2202794@gmail.com>
2025-12-08 21:29:32 +0700

    feat: implement gRPC user service with getUser method and update dependencies

--- backend/apps/api-gateway/src/app.controller.ts
+++ backend/apps/api-gateway/src/app.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ClientProxy } from '@nestjs/microservices';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common'
+import { AppService } from './app.service'
+import type { ClientGrpc } from '@nestjs/microservices'
+import { User, USER_SERVICE_NAME, UserServiceClient } from 'interfaces/user'
+import { lastValueFrom, Observable } from 'rxjs'
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit {
+  private userService: UserServiceClient
+  constructor(
+    private readonly appService: AppService,
+    @Inject(USER_SERVICE_NAME) private client: ClientGrpc,
+  ) {}
 
-  @Inject('USER_SERVICE')
-  private userService: ClientProxy;
-  @Get('users')
-  async getHello(): Promise<any> {
-    return this.userService.send('users', '');
+  onModuleInit() {
+    this.userService =
+      this.client.getService<UserServiceClient>(USER_SERVICE_NAME)
+  }
+
+  @Get('user')
+  async getHello(): Promise<User> {
+    const obs$ = this.userService.getUser({ id: '1' })
+    return await lastValueFrom(obs$)
   }
 }
